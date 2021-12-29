bd8ffe697843a186751567de5d9734c8613ec1b1
Kristian Øllegaard <kristian@kristian.io>
2021-12-29 14:31:52 +0000

    Revert "Somehow doesn't work without this initializer"
    This reverts commit fb72d38e5f9089b62378458272ab55ed799ace62.

--- custom_components/hass_stokercloud/__init__.py
+++ custom_components/hass_stokercloud/__init__.py
@@ -1,16 +1,3 @@
 """
 NBE Stoker Cloud
 """
-
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-from .const import DOMAIN, PLATFORMS
-from homeassistant.config_entries import ConfigEntry
-
-
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-
-    for platform in PLATFORMS:
-        hass.helpers.discovery.load_platform(platform, DOMAIN, {}, config)
-
-    return True
