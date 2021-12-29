199ae293b2c4a292d9a2f287de56fb046361be0f
Kristian Øllegaard <kristian@kristian.io>
2021-12-29 02:17:13 +0000

    Save data

--- custom_components/hass_stokercloud/config_flow.py
+++ custom_components/hass_stokercloud/config_flow.py
@@ -1,14 +1,24 @@
 import voluptuous as vol
 from .const import DOMAIN, DATA_SCHEMA
 from homeassistant import config_entries
-
+from homeassistant.const import CONF_USERNAME
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    async def async_step_user(self, info):
-        if info is not None:
-            pass  # TODO: process info
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                username = user_input[CONF_USERNAME]
+                info = f"Stoker Cloud {username}"
+                return self.async_create_entry(title=info, data=user_input)
+            except Exception:  # pylint: disable=broad-except
+                errors["base"] = "unknown"
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
