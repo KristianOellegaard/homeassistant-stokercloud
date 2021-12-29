59a3cb63284596b9611180f1844814732e66f4f8
Kristian Øllegaard <kristian@kristian.io>
2021-12-29 11:48:55 +0000

    Add missing method

--- custom_components/hass_stokercloud/__init__.py
+++ custom_components/hass_stokercloud/__init__.py
@@ -5,9 +5,21 @@ NBE Stoker Cloud
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from .const import DOMAIN, PLATFORMS
+from homeassistant.config_entries import ConfigEntry
+
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
 
     return True
