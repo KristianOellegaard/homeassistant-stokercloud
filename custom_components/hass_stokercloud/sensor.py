686900c3067a5118147fdcfe6ff98d1d1baf3947
nagels <nagels97@live.dk>
2024-12-28 01:27:50 +0100

    Update sensor.py
    Added Water heater current temp and requested temp as sensor.

--- custom_components/hass_stokercloud/sensor.py
+++ custom_components/hass_stokercloud/sensor.py
@@ -39,6 +39,9 @@ async def async_setup_entry(hass, config, async_add_entities):
         StokerCloudControllerSensor(client, serial, 'Boiler Effect', 'boiler_kwh', SensorDeviceClass.POWER),
         StokerCloudControllerSensor(client, serial, 'Total Consumption', 'consumption_total', state_class=SensorStateClass.TOTAL_INCREASING), # state class STATE_CLASS_TOTAL_INCREASING
         StokerCloudControllerSensor(client, serial, 'State', 'state'),
+
+        StokerCloudControllerSensor(client, serial, 'Current Water Heater Temperature', 'hotwater_temperature_current', SensorDeviceClass.TEMPERATURE),
+        StokerCloudControllerSensor(client, serial, 'Requested Water Heater Temperature', 'hotwater_temperature_requested', SensorDeviceClass.TEMPERATURE),
     ])
 
 
